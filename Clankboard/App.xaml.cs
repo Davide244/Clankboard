f577af729606d7eb9d44ca566ad2dc15e21f5b16
Davide_24 <69866130+Davide244@users.noreply.github.com>
2023-08-31 13:21:59 +0200

    Corrected wrong window title and added a minimum window size

--- Clankboard/App.xaml.cs
+++ Clankboard/App.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -30,17 +32,107 @@ public partial class App : Application
         this.InitializeComponent();
     }
 
-    public static ShellPage m_shellPage = new ShellPage();
-
     public static FrameworkElement MainRoot { get; private set; }
 
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
         m_window = new MainWindow();
         m_window.Activate();
-        m_window.Content = m_shellPage;
+        m_window.Content = new ShellPage();
+        m_window.Title = "Clankboard";
         MainRoot = m_window.Content as FrameworkElement;
+
+        RegisterWindowMinMax(m_window);
     }
 
     internal Window m_window;
+
+
+    // Override of the WndProc method to change the window's min size
+    #region WndProc
+    private static WinProc newWndProc = null;
+    private static IntPtr oldWndProc = IntPtr.Zero;
+    private delegate IntPtr WinProc(IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("User32.dll")]
+    internal static extern int GetDpiForWindow(IntPtr hwnd);
+    [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+    private static extern int SetWindowLong32(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc);
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+    private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc);
+    [DllImport("user32.dll")]
+    private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+    public static int MinWindowWidth { get; set; } = 497;
+    //public static int MaxWindowWidth { get; set; } = 1800;
+    public static int MinWindowHeight { get; set; } = 650;
+    //public static int MaxWindowHeight { get; set; } = 1600;
+
+    private static void RegisterWindowMinMax(Window window)
+    {
+        var hwnd = GetWindowHandleForCurrentWindow(window);
+
+        newWndProc = new WinProc(WndProc);
+        oldWndProc = SetWindowLongPtr(hwnd, WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
+    }
+
+    private static IntPtr GetWindowHandleForCurrentWindow(object target) =>
+        WinRT.Interop.WindowNative.GetWindowHandle(target);
+
+    private static IntPtr WndProc(IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam)
+    {
+        switch (Msg)
+        {
+            case WindowMessage.WM_GETMINMAXINFO:
+                var dpi = GetDpiForWindow(hWnd);
+                var scalingFactor = (float)dpi / 96;
+
+                var minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                minMaxInfo.ptMinTrackSize.x = (int)(MinWindowWidth * scalingFactor);
+                //minMaxInfo.ptMaxTrackSize.x = (int)(MaxWindowWidth * scalingFactor);
+                minMaxInfo.ptMinTrackSize.y = (int)(MinWindowHeight * scalingFactor);
+                //minMaxInfo.ptMaxTrackSize.y = (int)(MaxWindowHeight * scalingFactor);
+
+                Marshal.StructureToPtr(minMaxInfo, lParam, true);
+                break;
+
+        }
+        return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
+    }
+
+    private static IntPtr SetWindowLongPtr(IntPtr hWnd, WindowLongIndexFlags nIndex, WinProc newProc)
+    {
+        if (IntPtr.Size == 8)
+            return SetWindowLongPtr64(hWnd, nIndex, newProc);
+        else
+            return new IntPtr(SetWindowLong32(hWnd, nIndex, newProc));
+    }
+
+    private struct POINT
+    {
+        public int x;
+        public int y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
+    }
+
+    [Flags]
+    private enum WindowLongIndexFlags : int
+    {
+        GWL_WNDPROC = -4,
+    }
+
+    private enum WindowMessage : int
+    {
+        WM_GETMINMAXINFO = 0x0024,
+    }
+    #endregion
 }
