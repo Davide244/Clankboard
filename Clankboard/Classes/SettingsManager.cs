a975b46697ff7e7d7d5e1c9cfe8115bbf2bbec2d
Davide_24 <009davide@gmail.com>
2024-04-07 22:12:28 +0200

    Added robust settings system and settings UI overhaul.

---
+++ Clankboard/Classes/SettingsManager.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using static Clankboard.AudioManager;
+
+namespace Clankboard.Classes
+{
+    public static class SettingsManager
+    {
+        // Add a new enum to store the settings and their actual types
+        public enum SettingTypes
+        {
+            #region Audio Device Volume Settings
+            /// <summary>
+            /// Local output volume. This is the volume outputted to the local device.
+            /// </summary>
+            [SettingType(typeof(int))]
+            LocalOutputVolume,
+
+            /// <summary>
+            /// VAC output volume. This is the volume outputted to the virtual audio cable.
+            /// </summary>
+            [SettingType(typeof(int))]
+            VACOutputVolume,
+
+            /// <summary>
+            /// Microphone volume. This is the volume of the microphone.
+            /// </summary>
+            [SettingType(typeof(int))]
+            MicrophoneVolume,
+            #endregion
+
+            #region Audio Device Settings
+            [SettingType(typeof(AudioManager.AudioDevice))]
+            LocalOutputDevice,
+
+            [SettingType(typeof(AudioManager.AudioDevice))]
+            VACOutputDevice,
+
+            [SettingType(typeof(AudioManager.AudioDevice))]
+            InputDevice,
+            #endregion
+
+            /// <summary>
+            /// Output microphone to VAC. This is whether the microphone output is sent to the virtual audio cable.
+            /// </summary>
+            /// <remarks><b>AKA: </b>Audio mixer enabled for microphone.</remarks>
+            [SettingType(typeof(bool))]
+            OutputMicrophoneToVAC,
+
+            /// <summary>
+            /// Hear yourself enabled. This is whether the user can hear themselves through the microphone.
+            /// </summary>
+            [SettingType(typeof(bool))]
+            HearYourselfEnabled,
+
+            #region Soundboard Settings
+            [SettingType(typeof(bool))]
+            AudioStackingEnabled,
+
+            [SettingType(typeof(KeybindsManager.Keybind))]
+            StopAllSoundsKeybind,
+
+            [SettingType(typeof(bool))]
+            UseCompactSoundboardList,
+            #endregion
+        }
+
+        public static readonly AudioDevice DefaultAudioDevice = new AudioDevice { DeviceID = null, DeviceName = "DEFAULT" };
+
+        private static Dictionary<SettingTypes, object> settings = new Dictionary<SettingTypes, object>();
+        // Default values for the settings
+        private static Dictionary<SettingTypes, object> defaultSettings = new Dictionary<SettingTypes, object>
+        {
+            { SettingTypes.LocalOutputVolume, 100 },
+            { SettingTypes.VACOutputVolume, 100 },
+            { SettingTypes.MicrophoneVolume, 100 },
+            { SettingTypes.LocalOutputDevice, DefaultAudioDevice},
+            { SettingTypes.VACOutputDevice, DefaultAudioDevice },
+            { SettingTypes.InputDevice, DefaultAudioDevice },
+            { SettingTypes.OutputMicrophoneToVAC, true },
+            { SettingTypes.AudioStackingEnabled, false },
+            { SettingTypes.StopAllSoundsKeybind, null },
+            { SettingTypes.UseCompactSoundboardList, false },
+            { SettingTypes.HearYourselfEnabled, false },
+        };
+
+        public static void SetSetting<T>(SettingTypes name, T value)
+        {
+            if (typeof(T) != GetSettingType(name))
+            {
+                throw new InvalidCastException($"Setting '{name}' is not of type '{typeof(T)}'.");
+            }
+
+            settings[name] = value;
+
+            // Print to debug
+            Debug.WriteLine($"Setting '{name}' set to '{value}'");
+        }
+
+        public static T GetSetting<T>(SettingTypes name)
+        {
+            if (settings.ContainsKey(name))
+            {
+                return (T)settings[name];
+            }
+            else
+            {
+                //throw new KeyNotFoundException($"Setting '{name}' not found.");
+                // return empty value
+                return default(T);
+            }
+        }
+
+        public static bool SettingExists(SettingTypes name)
+        {
+            return settings.ContainsKey(name);
+        }
+
+        private static Type GetSettingType(SettingTypes name)
+        {
+            var field = typeof(SettingTypes).GetField(name.ToString());
+            var attribute = field.GetCustomAttributes(typeof(SettingTypeAttribute), false).FirstOrDefault() as SettingTypeAttribute;
+            return attribute?.Type;
+        }
+
+        public static object GetDefaultSettingValue(SettingTypes setting)
+        {
+            if (defaultSettings.ContainsKey(setting))
+            {
+                return defaultSettings[setting];
+            }
+            else
+            {
+                throw new KeyNotFoundException($"Default setting value for '{setting}' not found.");
+            }
+        }
+
+        /// <summary>
+        /// This function can do one thing:
+        /// Reset all settings to their default values
+        /// </summary>
+        public static void InitializeSettings()
+        {
+            foreach (SettingTypes setting in Enum.GetValues(typeof(SettingTypes)))
+            {
+                settings[setting] = GetDefaultSettingValue(setting);
+            }
+        }
+
+        /// <summary>
+        /// Attribute to store the type of the setting
+        /// </summary>
+        public class SettingTypeAttribute : Attribute
+        {
+            public Type Type { get; }
+
+            public SettingTypeAttribute(Type type)
+            {
+                Type = type;
+            }
+        }
+    }
+}
--- Clankboard/Pages/SettingsPage.xaml.cs
+++ Clankboard/Pages/SettingsPage.xaml.cs
@@ -12,20 +12,181 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using CommunityToolkit.WinUI.Controls;
+using static Clankboard.AudioManager;
+using Clankboard.Classes;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Clankboard.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page
     {
+
+        // List of audio output devices
+        private List<AudioDevice> AudioOutputDevices { get; set; } = new List<AudioDevice> { };
+        private List<AudioDevice> AudioInputDevices { get; set; } = new List<AudioDevice> { };
+
+        private bool SettingsEnabled = false;
+
+        public void UpdateComboboxes()
+        {
+            AudioOutputDevices = AudioManager.GetAudioOutputDevices();
+            AudioInputDevices = AudioManager.GetAudioInputDevices();
+
+            foreach (var device in AudioOutputDevices)
+            {
+                OutputDeviceCombobox.Items.Add(device.DeviceName);
+            }
+
+            foreach (var device in AudioInputDevices)
+            {
+                InputDeviceCombobox.Items.Add(device.DeviceName);
+                DriverInputCombobox.Items.Add(device.DeviceName);
+            }
+
+            string CurrentOutputDeviceID = SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.LocalOutputDevice).DeviceID;
+            string CurrentInputDeviceID = SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.InputDevice).DeviceID;
+            string CurrentDriverInputDeviceID = SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.VACOutputDevice).DeviceID;
+
+            // Set combobox indexes to the current audio devices
+            for (int i = 0; i < AudioOutputDevices.Count; i++)
+            {
+                if (AudioOutputDevices[i].DeviceID == CurrentOutputDeviceID)
+                {
+                    OutputDeviceCombobox.SelectedIndex = i + 1;
+                }
+            }
+
+            for (int i = 0; i < AudioInputDevices.Count; i++)
+            {
+                if (AudioInputDevices[i].DeviceID == CurrentInputDeviceID)
+                {
+                    InputDeviceCombobox.SelectedIndex = i + 1;
+                }
+                if (AudioInputDevices[i].DeviceID == CurrentDriverInputDeviceID)
+                {
+                    DriverInputCombobox.SelectedIndex = i;
+                }
+            }
+        }
+
+        private void UpdateToggleSwitches()
+        {
+            OutputMixerToggle.IsOn = SettingsManager.GetSetting<bool>(SettingsManager.SettingTypes.OutputMicrophoneToVAC);
+            HearYourselfToggle.IsOn = SettingsManager.GetSetting<bool>(SettingsManager.SettingTypes.HearYourselfEnabled);
+            StackedAudioToggle.IsOn = SettingsManager.GetSetting<bool>(SettingsManager.SettingTypes.AudioStackingEnabled);
+        }
+
         public SettingsPage()
         {
             this.InitializeComponent();
+            UpdateComboboxes();
+            UpdateToggleSwitches();
+
+            SettingsEnabled = true;
         }
+
+        // Event that covers the changing of the audio device comboboxes
+        private void AudioDeviceCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combobox = sender as ComboBox;
+            if (combobox.SelectedIndex == -1 || SettingsEnabled == false)
+            {
+                return;
+            }
+
+            if (combobox.Name == "OutputDeviceCombobox")
+            {
+                // Write previous output device to the debug log
+                Debug.WriteLine($"Previous Output Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.LocalOutputDevice).DeviceName}");
+
+                if (combobox.SelectedIndex == 0)
+                {
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.LocalOutputDevice, SettingsManager.DefaultAudioDevice);
+                }
+
+                if (combobox.SelectedIndex - 1 >= 0 && combobox.SelectedIndex - 1 <= AudioOutputDevices.Count)
+                {
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.LocalOutputDevice, new AudioDevice { DeviceName = AudioOutputDevices[combobox.SelectedIndex - 1].DeviceName, DeviceID = AudioOutputDevices[combobox.SelectedIndex - 1].DeviceID });
+                    Debug.WriteLine($"New Output Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.LocalOutputDevice).DeviceName}");
+                }
+            }
+            else if (combobox.Name == "InputDeviceCombobox")
+            {
+                // Write previous input device to the debug log
+                Debug.WriteLine($"Previous Input Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.InputDevice).DeviceName}");
+
+                if (combobox.SelectedIndex == 0)
+                {
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.InputDevice, SettingsManager.DefaultAudioDevice);
+                }
+
+                if (combobox.SelectedIndex - 1 >= 0 && combobox.SelectedIndex - 1 <= AudioInputDevices.Count)
+                {
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.InputDevice, new AudioDevice { DeviceName = AudioInputDevices[combobox.SelectedIndex - 1].DeviceName, DeviceID = AudioInputDevices[combobox.SelectedIndex - 1].DeviceID });
+                    Debug.WriteLine($"New Input Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.InputDevice).DeviceName}");
+                }
+            }
+            else if (combobox.Name == "DriverInputCombobox")
+            {
+                // Write previous driver input device to the debug log
+                Debug.WriteLine($"Previous Driver Input Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.VACOutputDevice).DeviceName}");
+
+                if (combobox.SelectedIndex >= 0 && combobox.SelectedIndex <= AudioInputDevices.Count)
+                {
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.VACOutputDevice, new AudioDevice { DeviceName = AudioInputDevices[combobox.SelectedIndex].DeviceName, DeviceID = AudioInputDevices[combobox.SelectedIndex].DeviceID });
+                    Debug.WriteLine($"New Driver Input Device: {SettingsManager.GetSetting<AudioDevice>(SettingsManager.SettingTypes.VACOutputDevice).DeviceName}");
+                }
+            }
+        }
+
+        private void SettingToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (!SettingsEnabled)
+                return;
+
+            var toggleSwitch = sender as ToggleSwitch;
+
+            switch(toggleSwitch.Name)
+            {
+                case "OutputMixerToggle":
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.OutputMicrophoneToVAC, toggleSwitch.IsOn);
+                    break;
+                case "HearYourselfToggle":
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.HearYourselfEnabled, toggleSwitch.IsOn);
+                    break;
+                case "StackedAudioToggle":
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.AudioStackingEnabled, toggleSwitch.IsOn);
+                    break;
+                case "UseCompactSoundboardListToggle":
+                    SettingsManager.SetSetting(SettingsManager.SettingTypes.UseCompactSoundboardList, toggleSwitch.IsOn);
+                    break;
+            }
+            //if (toggleSwitch.Name == "OutputMixerToggle")
+            //{
+            //    SettingsManager.SetSetting(SettingsManager.SettingTypes.OutputMicrophoneToVAC, toggleSwitch.IsOn);
+            //}
+            //else if (toggleSwitch.Name == "HearYourselfToggle")
+            //{
+            //    SettingsManager.SetSetting(SettingsManager.SettingTypes.HearYourselfEnabled, toggleSwitch.IsOn);
+            //}
+        }
+
+        //private void UseDefaultAudioDevicesBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    UpdateComboboxes();
+        //    OutputDeviceCombobox.SelectedIndex = 0;
+        //    InputDeviceCombobox.SelectedIndex = 0;
+        //    DriverInputCombobox.SelectedIndex = -1;
+        //}
+
+        //private void ApplyAudioDeviceChangesBtn_Click(object sender, RoutedEventArgs e)
+        //{
+            
+        //}
     }
 }
