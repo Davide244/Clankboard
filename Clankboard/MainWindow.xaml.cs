cb35e156cbb13b94c9c1a2985b52eb96498f57ad
Davide_24 <009davide@gmail.com>
2024-10-10 10:43:35 +0200

    Add project files.

---
+++ Clankboard/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Clankboard
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : WinUIEx.WindowEx
+    {
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            NavigationFrame.Navigate(typeof(Pages.SoundboardPage));
+
+            this.PersistenceId = "ClankMainWindow";
+
+            AppWindow appWindow = this.AppWindow;
+            appWindow.Title = "Clankboard";
+            appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+            appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+    }
+}
