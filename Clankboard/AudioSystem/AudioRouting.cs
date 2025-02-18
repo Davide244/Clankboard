d9d0e0740e83003fcad5dd6a9524d06be1241513
Davide_24 <009davide@gmail.com>
2025-02-18 11:19:38 +0100

    Added class for future audio routing code
    For more information, see audio routing diagram.

---
+++ Clankboard/AudioSystem/AudioRouting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clankboard.AudioSystem
+{
+    /// <summary>
+    /// Audio routing class.
+    /// Uses NAudio to route audio through the different systems inside of Clankboard.
+    /// </summary>
+    public class AudioRouting
+    {
+
+        // 3 NAudio mixer instances for the 2 different audio systems.
+        private NAudio.Wave.WaveMixerStream32 soundboardAudioMixer; // Mixes soundboard audio together into one stream.
+        private NAudio.Wave.WaveMixerStream32 mainOutputMixer;      // Mixes mixed soundboard audio with the microphonen for the main output.
+    }
+}
