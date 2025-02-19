b9c01bc40ab85dd97a60ffac004790f606208b0c
Davide_24 <009davide@gmail.com>
2025-02-19 16:41:42 +0100

    Started the move back to ListView.

--- Clankboard/Pages/SoundboardPage.xaml.cs
+++ Clankboard/Pages/SoundboardPage.xaml.cs
@@ -40,14 +40,14 @@ namespace Clankboard.Pages
             soundBoard.soundboardViewmodel.SoundboardItems.CollectionChanged += SoundboardItems_CollectionChanged;
 
             // Add random soundboard items for testing
-            //soundBoard.Add(new SoundboardItem("Test Item", @"C:\Windows\Windows.mp3", SoundboardItemType.LocalFile, "", true, false));
-            //soundBoard.Add(new SoundboardItem("Test Item 2", @"C:\Windows\Windows.mp3", SoundboardItemType.LocalFile, "", true, false, true));
-            //soundBoard.Add(new SoundboardItem("Test Item with really really looooooooong name oooo soo long", @"C:\Users\Really\Long\File\Path\That\Exceeds\The\Max\Width\Of\Display.mp3", SoundboardItemType.LocalFile, "", true, false));
-            //soundBoard.Add(new SoundboardItem("Test Item Downloaded Item", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", true, false, false));
-            //soundBoard.Add(new SoundboardItem("Test Item Downloading Item", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", false, true, false));
-            //soundBoard.Add(new SoundboardItem("Test Item Downloaded Item w/ Errors", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", true, false, true));
-            //soundBoard.Add(new SoundboardItem("Test TTS Item", "Hi, This is some test TTS Text!", SoundboardItemType.TTSFile, "", true, false, false));
-            //soundBoard.Add(new SoundboardItem("Test TTS Item w/ Errors", "Hi, This is some test TTS Text!", SoundboardItemType.TTSFile, "", true, false, true));
+            soundBoard.Add(new SoundboardItem("Test Item", @"C:\Windows\Windows.mp3", SoundboardItemType.LocalFile, "", true, false));
+            soundBoard.Add(new SoundboardItem("Test Item 2", @"C:\Windows\Windows.mp3", SoundboardItemType.LocalFile, "", true, false, true));
+            soundBoard.Add(new SoundboardItem("Test Item with really really looooooooong name oooo soo long", @"C:\Users\Really\Long\File\Path\That\Exceeds\The\Max\Width\Of\Display.mp3", SoundboardItemType.LocalFile, "", true, false));
+            soundBoard.Add(new SoundboardItem("Test Item Downloaded Item", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", true, false, false));
+            soundBoard.Add(new SoundboardItem("Test Item Downloading Item", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", false, true, false));
+            soundBoard.Add(new SoundboardItem("Test Item Downloaded Item w/ Errors", @"https://www.youtube.com/watch?v=WyQ7z8BMwwk", SoundboardItemType.DownloadedFile, "", true, false, true));
+            soundBoard.Add(new SoundboardItem("Test TTS Item", "Hi, This is some test TTS Text!", SoundboardItemType.TTSFile, "", true, false, false));
+            soundBoard.Add(new SoundboardItem("Test TTS Item w/ Errors", "Hi, This is some test TTS Text!", SoundboardItemType.TTSFile, "", true, false, true));
         }
 
         private void SoundboardItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
